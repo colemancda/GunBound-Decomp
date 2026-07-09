432adefd5d127746838c6d51f3ea4e5a0532809e
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-08 22:03:40 -0400

    Fix FUN_004b1810.c raw port for the MSVC syntax check Rewrite Ghidra's ._<off>_<size>_ subfield accesses with the SUBFIELD() macro from ghidra_types.h.

--- src/unnamed/FUN_004b1810.c
+++ src/unnamed/FUN_004b1810.c
@@ -372,7 +372,7 @@ LAB_004b1bd5:
         uStack_8a4 = 0;
         iStack_ab0 = 0;
         EncodeOutgoingPacketField(0);
-        puStack_8._0_1_ = 4;
+        SUBFIELD(puStack_8,0,undefined1) = 4;
         FUN_004262d0(iStack_ac8 + 0x10,auStack_ac4);
         EnterCriticalSection((LPCRITICAL_SECTION)&DAT_005a9068);
         PeekPacketChecksumState();
@@ -387,7 +387,7 @@ LAB_004b1bd5:
         uVar18 = 0;
         FUN_004065a0(uVar8,0,100,0xff,iVar5,0);
         FUN_00432320((char)param_1[0xf],1,1,uVar8,uVar18,uVar19,uVar20,iVar5,uVar21);
-        puStack_8 = (undefined1 *)CONCAT31(puStack_8._1_3_,3);
+        puStack_8 = (undefined1 *)CONCAT31(SUBFIELD(puStack_8,1,undefined3),3);
         FUN_0040a2a0();
 LAB_004b1e9d:
         puStack_8 = (undefined1 *)0xffffffff;
@@ -403,7 +403,7 @@ LAB_004b1e9d:
           uStack_680 = 0;
           uStack_88c = 0;
           EncodeOutgoingPacketField(0);
-          puStack_8._0_1_ = 2;
+          SUBFIELD(puStack_8,0,undefined1) = 2;
           QueueOutgoingPacketField(uStack_adc);
           QueueOutgoingPacketField(local_ad4);
           FUN_004262d0(iStack_ac8 + 0x10,auStack_8a0);
@@ -416,7 +416,7 @@ LAB_004b1e9d:
           uVar8 = FUN_0040a4d0(auStack_ac4);
           FUN_004065a0(uVar8,uVar18,uVar19,uVar20,iVar5,uVar21);
           FUN_00432320((char)param_1[0xf],1,1,uVar8,uVar18,uVar19,uVar20,iVar5,uVar21);
-          puStack_8 = (undefined1 *)CONCAT31(puStack_8._1_3_,1);
+          puStack_8 = (undefined1 *)CONCAT31(SUBFIELD(puStack_8,1,undefined3),1);
           FUN_0040a2a0();
           goto LAB_004b1e9d;
         }
